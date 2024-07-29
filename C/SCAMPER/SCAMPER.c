170d19a0188909743f0610685ba733e744823403
nhyun199 <shgus1692@gmail.com>
2024-07-29 18:16:00 +0900

    Assignment - SCMPER Function
    SCMPER function implement.
    Substitute, Combine, Adjust / Adapt, Modify / Maginify / Minify,
    Put to another use, Eliminate, Reverse / Rearrange.

---
+++ C/SCAMPER/SCAMPER.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	// SCAMPER 함수 구현해보기, pointer를 사용해보기
+	// substitute combine adjust/adapt ... 
+
+	// Substitute - 문자열 대체하기
+
+	// Combine - 문자열 합치기
+
+	// Adjust, Adapt - 원본 문자열 변경
+
+	// Maginify, Minify - 대소문자 변환
+
+	// Put to another use - P로 시작하는 문자열 관련 함수 만들기
+
+	// Eliminate - 특정 문자열 제거하기
+
+	// Reverse, Rearrange - 문자열 순서 뒤집기
+}
