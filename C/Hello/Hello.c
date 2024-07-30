c75dc98cdbede88db64e10b52229cfca702e8f01
nhyun199 <shgus1692@gmail.com>
2024-07-30 17:13:31 +0900

    Add Pratice Solution / Update Hello

--- C/Hello/Hello.c
+++ C/Hello/Hello.c
@@ -5,17 +5,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include "../LibTest/LibTest.h"
+//#pragma comment(lib, "./x64/Debug/LibTest") //
 
 #define LOTTO_MAX_NUM 45
 #define LOTTO_NUM_COUNT 6
 #define TRIALS 1000000
-
-int AtSign(int A, int B)
-{
-	int C;
-	C = (A + B) * (A - B);
-	return C;
-}
 
 void generateLottoNumbers(int* lottoNumbers, int count)
 {
@@ -115,94 +109,7 @@ void main()
 	printf("\nmax = %d, min = %d, average = %.1f", max, min, (float)sum / 10);*/
 
 	//printf("총합은 %d입니다",sum);
-
-
-	// 구구단 출력하기
-	/*int N;
-	scanf_s("%d", &N);
-	for (int i = 1; i < 10; i++)
-	{
-		printf("%d * %d = %d\n", N, i, N * i);
-	}*/
-
-	// A + B 반복 출력하기
-
-	/*int T, A, B;
-	scanf_s("%d", &T);
-	for (int i = 0; i < T; i++)
-	{
-		scanf_s("%d %d", &A, &B);
-		printf("%d\n", A + B);
-	}*/
-
-	// 1부터 N까지의 합 출력하기
-
-	/*int N;
-	int Sum = 0;
-	scanf_s("%d", &N);
-
-	for (int i = 1; i <= N; i++)
-	{
-		Sum += i;
-	}
-	printf("%d", Sum);*/
-
-	// 공백 4 3 2 1 0 별 1 2 3 4 5 
-	/*int N;
-	scanf_s("%d", &N);
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N - (i + 1); j++)
-		{
-			printf(" ");
-		}
-		for (int k = 0; k < i; k++)
-		{
-			printf("*");
-		}		
-		printf("*\n");
-	}*/
-
-	/*int A, B;
-	for ( ; ; )
-	{
-		scanf_s("%d %d", &A, &B);
-		if (A != 0 || B != 0)
-		{
-			printf("%d\n", A + B);
-		}
-		else
-			break;
-	}*/
-
-	/*int x, y;
-	for (; ; )
-	{
-		if (scanf("%d %d", &x, &y) == EOF)
-			break;
-
-		printf("%d", x + y);
-	}*/
-
-	/*int a;
-	printf("\\    /\\\n");
-	printf(" )  ( ')\n");
-	printf("(  /  )\n");
-	printf(" \\(__)|\n");
-	printf("|\\_/|\n");
-	printf("|q p|   /\}\n");
-	printf("( 0 )\"\"\"\\\n");
-	printf("|\"^\"`    |\n");
-	printf("||_/=\\\\__|\n");
-	printf("         ,r'\"7\n");
-	printf("r`-_   ,'  ,/\n");
-	printf(" \\. \". L_r'\n");
-	printf("   `~\\/\n");
-	printf("      |\n");
-	printf("      |\n");*/
-
-	
+		
 	// 1 ~ 45 6개 선택
 	// 중복 X
 	
@@ -229,36 +136,11 @@ void main()
 	//	if (N[i] == 0)
 	//		printf("%d\n", i);
 	//}
-
-	/*int N, M;
-	int A[100][100], B[100][100];
-	scanf_s("%d %d", &N, &M);
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)		
-			scanf_s("%d", &A[i][j]);		
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-			scanf_s("%d", &B[i][j]);
-	}
 	
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			printf("%d ", A[i][j] + B[i][j]);
-		}
-		printf("\n");
-	}*/
-
 	/*char X;
 	scanf_s("%c", &X);
 	printf("%d", X);*/
-		
+	
 	/*char* apszData[] = { "Test", "String", "Data" };
 	char szBuffer[128] = { 0 };
 	int i = 0, nOffset = 0;*/
@@ -285,109 +167,7 @@ void main()
 		printf("합이 %d가 나올 확률은 %.5f%%입니다.\n", i, (double)cnt[i] / TRIALS * 100);
 	}*/
 	
-	/*char grade;
-	char sign;
-
-	scanf_s("%c", &grade);
-	if (grade == 'F')
-	{
-		printf("0.0");
-		return 0;
-	}
-
-	scanf_s("%c", &sign);
-
-	switch (grade)
-	{
-	case 'A' :
-		switch (sign)
-		{
-		case '+' :
-			printf("4.3");
-			break;
-		case '0' :
-			printf("4.0");
-			break;
-		case '-' :
-			printf("3.7");
-			break;
-		}
-		break;
-	case 'B':
-		switch (sign)
-		{
-		case '+':
-			printf("3.3");
-			break;
-		case '0':
-			printf("3.0");
-			break;
-		case '-':
-			printf("2.7");
-			break;
-		}
-		break;
-	case 'C':
-		switch (sign)
-		{
-		case '+':
-			printf("2.3");
-			break;
-		case '0':
-			printf("2.0");
-			break;
-		case '-':
-			printf("1.7");
-			break;
-		}
-		break;
-	case 'D':
-		switch (sign)
-		{
-		case '+':
-			printf("1.3");
-			break;
-		case '0':
-			printf("1.0");
-			break;
-		case '-':
-			printf("0.7");
-			break;
-		}
-		break;
-	}*/
-
-	/*char S[1001];
-	int i;
-
-	scanf("%s", S);
-	scanf("%d", &i);
-
-	printf("%c", S[i-1]);*/
-
-	/*int T;
-	char S[1001];
-	
-	scanf("%d", &T);
-
-	for (int i = 0; i < T; i++)
-	{
-		scanf("%s", S);
-		int len = strlen(S);
-
-
-		if (len == 1)
-			printf("%c%c\n", S[0], S[0]);
-		else if (len > 0)
-			printf("%c%c\n", S[0], S[len - 1]);
-	}*/
-
-	/*int A, B;
-	scanf("%d %d", &A, &B);
-	printf("%d", AtSign(A, B));*/
-	
+	// 정적 라이브러리
 	PrintData(10);
-	PrintString("hello");
-
-	
+	PrintString("hello");	
 }
---
+++ Practice/Practice.c
@@ -0,0 +1,249 @@
+﻿#include <stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int AtSign(int A, int B)
+{
+	int C;
+	C = (A + B) * (A - B);
+	return C;
+}
+
+int main()
+{
+	/*min = a[0];
+	max = a[0];
+
+	for (int i = 1; i < 10; i++)
+	{
+		if (min > a[i])
+			min = a[i];
+
+		if (max < a[i])
+			max = a[i];
+	}
+	
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", a[i]);
+	}
+
+	printf("\nmax = %d, min = %d, average = %.1f", max, min, (float)sum / 10);*/
+
+	//printf("총합은 %d입니다",sum);
+
+
+	// 구구단 출력하기
+	/*int N;
+	scanf_s("%d", &N);
+	for (int i = 1; i < 10; i++)
+	{
+		printf("%d * %d = %d\n", N, i, N * i);
+	}*/
+
+	// A + B 반복 출력하기
+
+	/*int T, A, B;
+	scanf_s("%d", &T);
+	for (int i = 0; i < T; i++)
+	{
+		scanf_s("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}*/
+
+	// 1부터 N까지의 합 출력하기
+
+	/*int N;
+	int Sum = 0;
+	scanf_s("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+	{
+		Sum += i;
+	}
+	printf("%d", Sum);*/
+
+	// 공백 4 3 2 1 0 별 1 2 3 4 5 
+	/*int N;
+	scanf_s("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N - (i + 1); j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i; k++)
+		{
+			printf("*");
+		}		
+		printf("*\n");
+	}*/
+
+	/*int A, B;
+	for ( ; ; )
+	{
+		scanf_s("%d %d", &A, &B);
+		if (A != 0 || B != 0)
+		{
+			printf("%d\n", A + B);
+		}
+		else
+			break;
+	}*/
+
+	/*int x, y;
+	for (; ; )
+	{
+		if (scanf("%d %d", &x, &y) == EOF)
+			break;
+
+		printf("%d", x + y);
+	}*/
+
+	// 그림 출력하기 - 특이한 문자 출력
+	/*
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+	printf("|\\_/|\n");
+	printf("|q p|   /\}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");*/
+
+	// 학점 계산기
+	/*char grade;
+	char sign;
+
+	scanf_s("%c", &grade);
+	if (grade == 'F')
+	{
+		printf("0.0");
+		return 0;
+	}
+
+	scanf_s("%c", &sign);
+
+	switch (grade)
+	{
+	case 'A' :
+		switch (sign)
+		{
+		case '+' :
+			printf("4.3");
+			break;
+		case '0' :
+			printf("4.0");
+			break;
+		case '-' :
+			printf("3.7");
+			break;
+		}
+		break;
+	case 'B':
+		switch (sign)
+		{
+		case '+':
+			printf("3.3");
+			break;
+		case '0':
+			printf("3.0");
+			break;
+		case '-':
+			printf("2.7");
+			break;
+		}
+		break;
+	case 'C':
+		switch (sign)
+		{
+		case '+':
+			printf("2.3");
+			break;
+		case '0':
+			printf("2.0");
+			break;
+		case '-':
+			printf("1.7");
+			break;
+		}
+		break;
+	case 'D':
+		switch (sign)
+		{
+		case '+':
+			printf("1.3");
+			break;
+		case '0':
+			printf("1.0");
+			break;
+		case '-':
+			printf("0.7");
+			break;
+		}
+		break;
+	}*/
+
+	/*int T;
+	char S[1001];
+
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%s", S);
+		int len = strlen(S);
+
+		if (len == 1)
+				printf("%c%c\n", S[0], S[0]);
+		else if (len > 0)
+			printf("%c%c\n", S[0], S[len - 1]);
+	}*/
+
+	
+	/*int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", AtSign(A, B));*/
+
+	/*char S[1001];
+	int i;
+
+	scanf("%s", S);
+	scanf("%d", &i);
+
+	printf("%c", S[i-1]);*/
+
+	/*int N, M;
+	int A[100][100], B[100][100];
+	scanf_s("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			scanf_s("%d", &A[i][j]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			scanf_s("%d", &B[i][j]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			printf("%d ", A[i][j] + B[i][j]);
+		}
+		printf("\n");
+	}*/
+
+}
