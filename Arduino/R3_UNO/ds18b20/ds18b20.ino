fe95edaa8932a641a721b950358a1b960b10dbc2
nhyun199 <shgus1692@gmail.com>
2024-08-05 15:10:04 +0900

    New sensor setting test

---
+++ Arduino/R3_UNO/ds18b20/ds18b20.ino
@@ -0,0 +1,24 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  sensors.begin();
+}
+
+void loop(void)
+{
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  Serial.print("Temperature for the device 1 (index 0) is: ");
+  Serial.println(sensors.getTempCByIndex(0));
+}
