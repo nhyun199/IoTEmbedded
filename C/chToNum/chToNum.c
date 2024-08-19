e22404449f41c9d27e9093497dca8cac58dc6f10
nhyun199 <shgus1692@gmail.com>
2024-08-19 18:16:55 +0900

    Update chToNum.c

--- C/chToNum/chToNum.c
+++ C/chToNum/chToNum.c
@@ -10,7 +10,7 @@ double chToNum(char* s)
 	double n = 0;
 	int len = strlen(s);
 	int minus = -1;
-	int pointIndex = 1;
+	int point = 1;
 	for (int i = 0; i < len; i++)
 	{
 		if (isdigit(s[i]))
@@ -19,18 +19,24 @@ double chToNum(char* s)
 			{
 				n += (s[i] - 48) * pow(10, len - i - 1);
 			}
-			else if (s[i] == '.')
+			else if (s[i] == '.') 
 			{
-				pointIndex = pow(10, (len - (i+1)))
+				point = pow(10, -(len-(i+1)));
 			}
 		}
-		
+		// 1.234
+		// 30.5678
+		// len = 7 point = 3 
+		// len = 5 , point = 2 5-2 = 3 1234 * 10^-3
+		// point = pow(10, -(len-point))
+		// 1.234 s[0] = 1 /s[1] = . /s[2] = 2 /s[3] = 3 /s[4] = 4
+		// i = 1 -> point 변화 생기고. 
 	}
 
-	if (s[0] == '-');
-	return -n * pointIndex;
+	if (s[0] == '-')
+		return -n * point;
 
-	return n * pointIndex;
+	return n * point;
 }
 
 int main()
