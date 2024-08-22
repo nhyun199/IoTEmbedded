81254ba63d269bc5cdaa3dce50ebced97c5e653a
nhyun199 <shgus1692@gmail.com>
2024-08-22 17:40:46 +0900

    study queue

---
+++ QUEUE/queue.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+void push(int **queue, int *back, int X) 
+{
+	if (*queue == NULL)
+	{
+		*queue = (int*)malloc(sizeof(int));
+	}
+	else
+	{
+		*queue = (int*)realloc(*queue, (*back + 1) * sizeof(int));
+	}
+	(*queue)[*back] = X;
+	(*back)++;
+}
+
+void pop() {}
+void size() {}
+void empty() {}
+void Front() {}
+void Back() {}
+
+int main()
+{
+	//int N;
+	int* queue = NULL;
+	int front = 0;
+	int back = 0;
+}
