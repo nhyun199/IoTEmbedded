2a71e03367bb3cf0904d93ed5dd012b781b5161f
nhyun199 <shgus1692@gmail.com>
2024-09-02 12:21:26 +0900

    update

---
+++ ExamSolution/reverse2/reverse2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char S[100001];
+	scanf("%s", S);
+	int len = strlen(S);
+
+	for (int i = 0; i < len; i++)
+	{
+		
+	}
+}
