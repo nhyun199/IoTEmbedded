7aa6ec7f8c23ebd9a80340b697bdbdcc6dc06e8e
nhyun199 <shgus1692@gmail.com>
2024-09-06 12:29:34 +0900

    update

---
+++ Algorithm/bigIntegerArithmetic/bigIntegerArithmetic.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	char num1[] = "10";
+	char num2[] = "25";
+
+	// 문자형식의 수에서 '0'을 빼면 정수형이 된다.
+	// ex) 
+}
