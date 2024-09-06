e3443b8fe73fa82410e61d2421484db0e22fcb11
nhyun199 <shgus1692@gmail.com>
2024-09-06 10:00:33 +0900

    update

--- ExamSolution/throwBall/throwBall.c
+++ ExamSolution/throwBall/throwBall.c
@@ -1,15 +1,52 @@
-#define _CRT_SECURE_NO_WARNINGS
+癤�#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
 	int N, M, L;
+	int i = 0;
 	scanf("%d %d %d", &N, &M, &L);
 
-	int* count = (int*)malloc(N * sizeof(int));
+	int* count = (int*)calloc(N, sizeof(int));
+	count[0] = 1;
+	int throw = 1;
 
-	// 1번부터 던짐 -> count[0]++
-	// 한 사람이 M번 받으면 게임 끝
-	
+	while (1)
+	{
+		if (M == 1)
+		{
+			printf("0");
+			free(count);
+			return 0;
+		}
+
+		if (count[i] % 2 != 0)
+		{
+			if (i + L > N - 1)
+				i = i + L - N;
+			else
+				i = i + L;
+
+			count[i]++;
+		}
+		else if(count[i] % 2 == 0)
+		{
+			if (i < L)
+				i = N - (L - i);
+			else
+				i = i - L;
+
+			count[i]++;
+		}
+
+		if (count[i] == M)
+		{
+			printf("%d", throw);
+			free(count);
+			return 0;
+		}
+
+		throw++;
+	}
 }
